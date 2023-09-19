2457fd914de6da98ee697d5e5edf48a157f7492a
Pablo T. de Vargas <pablotdvsm@gmail.com>
2023-09-19 10:50:51 -0300

    moved to app router

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from 'next/image'
+import Button from '@mui/material/Button';
+
+export default function Home() {
+  return (
+    <>
+      <Button variant='contained'>Hello world</Button>
+    </>
+  )
+}
